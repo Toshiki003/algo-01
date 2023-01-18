c7b63c2ae0daf177c2d044fc274f070aa9319627
関目智史 <zou3road@gmail.com>
2023-01-19 06:41:13 +0900

    array

--- array_string01.rb
+++ array_string01.rb
@@ -1,12 +1,17 @@
+# 文字列を受け取る　chompで改行をカット
 S = gets.chomp
 c = gets.chomp
 
+# Sの長さを取得しNに格納
 N = S.size
 
+# 線形探索用のフラグ
 flag = false
 
+N回繰り返し
 N.times do |i|
   flag = true if S[i] == c
 end
 
+# 三項演算子
 puts flag ? "Yes" : "No"
